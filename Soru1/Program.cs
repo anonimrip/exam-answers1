8b2b55bd27f853405f41c0dd346d6dc9330835ee
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-05-12 11:24:57 +0300

    Proje dosyası ekle.

---
+++ Soru1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//ER3N
+namespace Soru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad, soyad;
+            int num;
+
+            Console.Write("   Adınız: ");
+            ad = Console.ReadLine();
+            Console.Write("Soyadınız: ");
+            soyad = Console.ReadLine();
+            Console.Write("       NO: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("   Adınız: " + ad);
+            Console.WriteLine("Soyadınız: " + soyad);
+            Console.WriteLine("       NO: " + num);
+            Console.WriteLine();
+            Console.WriteLine("HOŞGELDİNİZ " + num + " NUMARALI " + ad + " " + soyad);
+
+        }
+    }
+}
